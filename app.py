46c9ffb435e9251ee8e9d02eef35b73770d04bab
saipavan-p pilla <pillasaipavan94@gmail.com>
2023-06-16 12:47:17 +0000

    bot

---
+++ app.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI 
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+app.mount("/static",StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+def read_root():
+    with open('templates/project.html','r') as file:
+        content = file.read()
+    return HTMLResponse(content=content)
+
+        
+        
