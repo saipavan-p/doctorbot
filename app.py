a3f1f8c9d8d80a148897393a8666a3f4a095908c
saipavan-p pilla <pillasaipavan94@gmail.com>
2023-06-20 10:53:33 +0000

    v3

--- app.py
+++ app.py
@@ -1,86 +1,86 @@
-# from fastapi import FastAPI 
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.responses import HTMLResponse
-
-# app = FastAPI()
-# app.mount("/static",StaticFiles(directory="static"), name="static")
-
-# @app.get("/")
-# def read_root():
-#     with open('templates/project.html','r') as file:
-#         content = file.read()
-#     return HTMLResponse(content=content)
-
-from fastapi import FastAPI
+from fastapi import FastAPI 
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
-from fastapi import Request
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
-import openai
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+app.mount("/static",StaticFiles(directory="static"), name="static")
 
-# Set your OpenAI API key
-openai.api_key = "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"
+@app.get("/")
+def read_root():
+    with open('templates/project.html','r') as file:
+        content = file.read()
+    return HTMLResponse(content=content)
 
-class ChatInput(BaseModel):
-    input: str
+# from fastapi import FastAPI
+# from fastapi.staticfiles import StaticFiles
+# from fastapi.responses import HTMLResponse
+# from fastapi import Request
+# from fastapi.templating import Jinja2Templates
+# from pydantic import BaseModel
+# import openai
 
-class CreateBot:
+# app = FastAPI()
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+# templates = Jinja2Templates(directory="templates")
 
-    def __init__(self, system_prompt):
-        self.system = system_prompt
-        self.messages = [{"role": "system", "content": system_prompt}]
+# # Set your OpenAI API key
+# openai.api_key = "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"
+
+# class ChatInput(BaseModel):
+#     input: str
+
+# class CreateBot:
+
+#     def __init__(self, system_prompt):
+#         self.system = system_prompt
+#         self.messages = [{"role": "system", "content": system_prompt}]
 
 
-    def chat(self):
-        '''
-        Tracks dialogue history and takes in user input
-        '''
-        # print('Enter "Take my medication status" to give details of medications')
-        # print()
-        print("Hello dear patient. I'm your Online Consultant Dr.John.")
-        print()
-        question = ''
-        while question != 'END':
-            # Get User Question
-            question = input("")
+#     def chat(self):
+#         '''
+#         Tracks dialogue history and takes in user input
+#         '''
+#         # print('Enter "Take my medication status" to give details of medications')
+#         # print()
+#         print("Hello dear patient. I'm your Online Consultant Dr.John.")
+#         print()
+#         question = ''
+#         while question != 'END':
+#             # Get User Question
+#             question = input("")
 
-            # Add to messages/dialogue history
-            self.messages.append({'role': 'user', 'content': question})
+#             # Add to messages/dialogue history
+#             self.messages.append({'role': 'user', 'content': question})
 
-            # Send to ChatGPT and get response
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=self.messages
-            )
+#             # Send to ChatGPT and get response
+#             response = openai.ChatCompletion.create(
+#                 model="gpt-3.5-turbo",
+#                 messages=self.messages
+#             )
 
-            # Get content of assistant reply
-            content = response['choices'][0]['message']['content']
-            print('\n')
-            print(content)
-            print('\n')
+#             # Get content of assistant reply
+#             content = response['choices'][0]['message']['content']
+#             print('\n')
+#             print(content)
+#             print('\n')
             
-            # Add assistant reply for dialogue history
-            self.messages.append({'role': 'assistant', 'content': content})
+#             # Add assistant reply for dialogue history
+#             self.messages.append({'role': 'assistant', 'content': content})
 
 
-@app.get("/", response_class=HTMLResponse)
-def read_root(request: Request):
-    return templates.TemplateResponse("project.html", {"request": request})
+# @app.get("/", response_class=HTMLResponse)
+# def read_root(request: Request):
+#     return templates.TemplateResponse("project.html", {"request": request})
 
-@app.post("/chatbot")
-def chat_with_bot(chat_input: ChatInput):
-    input_text = chat_input.input.strip()
-    doctor = CreateBot(system_prompt=input_text)
-    response = doctor.chat()
-    return {"response": response}
+# @app.post("/chatbot")
+# def chat_with_bot(chat_input: ChatInput):
+#     input_text = chat_input.input.strip()
+#     doctor = CreateBot(system_prompt=input_text)
+#     response = doctor.chat()
+#     return {"response": response}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
 
     
--- static/script1.js
+++ static/script1.js
@@ -1,90 +1,122 @@
-// Function to handle user login
-function handleLogin(event) {
-  event.preventDefault();
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
+document.addEventListener("DOMContentLoaded", function() {
+  const loginContainer = document.getElementById("login-container");
+  const chatContainer = document.getElementById("chat-container");
+  const loginForm = document.getElementById("login-form");
+  const usernameInput = document.getElementById("username");
+  const passwordInput = document.getElementById("password");
+  const chatLog = document.getElementById("chat-log");
+  const userInput = document.getElementById("user-input");
+  const recommendationList = document.getElementById("recommendation-list");
+  const enterButton = document.getElementById("enter-button");
 
-  // Perform login validation and redirect if successful
-  // ...
+  // Function to add a new message to the chat log
+  function addMessage(message, sender) {
+    const messageContainer = document.createElement("div");
+    messageContainer.classList.add("message");
+    messageContainer.classList.add(sender);
 
-  // Clear the input fields
-  document.getElementById("username").value = "";
-  document.getElementById("password").value = "";
-}
+    const messageCard = document.createElement("p");
+    messageCard.classList.add("message-card");
+    messageCard.textContent = message;
 
-// Function to add a new message to the chat log
-function addMessage(role, content) {
-  var chatLog = document.getElementById("chat-log");
-  var message = document.createElement("div");
-  message.className = role;
-  message.innerText = content;
-  chatLog.appendChild(message);
-}
-
-// Function to handle recommended message click
-function handleRecommendationClick(event) {
-  var userInput = document.getElementById("user-input");
-  var recommendedMessage = event.target.innerText;
-  userInput.value = recommendedMessage;
-}
-
-// Event listener for login form submission
-var loginForm = document.getElementById("login-form");
-loginForm.addEventListener("submit", handleLogin);
-
-// Event listener for user input
-var userInput = document.getElementById("user-input");
-userInput.addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    var message = userInput.value;
-    addMessage("user", message);
-    userInput.value = "";
-
-    // Call the function to handle user input and get the response from the bot
-    handleUserInput(message);
+    messageContainer.appendChild(messageCard);
+    chatLog.appendChild(messageContainer);
+    chatLog.scrollTop = chatLog.scrollHeight;
   }
-});
 
-// Event listener for recommended message click
-var recommendationList = document.getElementById("recommendation-list");
-recommendationList.addEventListener("click", handleRecommendationClick);
+  // Function to handle user login
+  function handleLogin(event) {
+    event.preventDefault();
 
-// Event listener for Enter button click
-var enterButton = document.getElementById("enter-button");
-enterButton.addEventListener("click", function(event) {
-  var message = userInput.value;
-  addMessage("user", message);
-  userInput.value = "";
+    const username = usernameInput.value;
+    const password = passwordInput.value;
 
-  // Call the function to handle user input and get the response from the bot
-  handleUserInput(message);
-});
-
-// Function to handle user input
-function handleUserInput(input) {
-  // Create a new XMLHttpRequest object
-  var xhr = new XMLHttpRequest();
-
-  // Set up the request
-  xhr.open("POST", "/chatbot", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  // Set up the callback function for when the request completes
-  xhr.onload = function() {
-    if (xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText).response;
-      addMessage("assistant", response);
+    if (username === "admin" && password === "password") {
+      loginContainer.style.display = "none";
+      chatContainer.style.display = "block";
     } else {
-      console.log("Error:", xhr.status);
+      alert("Invalid username or password. Please try again.");
     }
-  };
 
-  // Create the request body as JSON
-  var requestBody = JSON.stringify({ input: input });
+    usernameInput.value = "";
+    passwordInput.value = "";
+  }
 
-  // Send the request
-  xhr.send(requestBody);
-}
+  // // Function to handle user input
+  // function handleUserInput() {
+  //   const message = userInput.value;
 
+  //   if (message.trim() !== "") {
+  //     addMessage(message, "user");
+  //     // Process user message and generate response
+  //     const response = getResponse(message);
+  //     setTimeout(function() {
+  //       addMessage(response, "bot");
+  //     }, 500);
 
+  //     userInput.value = "";
+  //   }
+  // }
+  async function handleUserInput() {
+    const u_message = userInput.value;
+    // Create an array to store the dialogue history/messages
+    let messages = [{ role: 'system', content: 'You are Dr.John, a doctor interacting with your patient. First, greet the patient and then ask for their problems. Respond in a short and sweet manner.' }];
+  
+    // Add user input to the dialogue history
+    messages.push({ role: 'user', content: u_message });
+  
+    try {
+      // Send HTTP POST request to the OpenAI GPT-3 API
+      const response = await axios.post('https://api.openai.com/v1/chat/completions', {
+        model: 'gpt-3.5-turbo',
+        messages: messages
+      }, {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': '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' // Replace with your OpenAI API key
+        }
+      });
+  
+      // Get the content of the assistant's reply
+      const content = response.data.choices[0].message.content;
+  
+      // Add the assistant's reply to the chat log
+      addMessage('assistant', content);
+    } catch (error) {
+      console.error('Error:', error);
+      addMessage('assistant', 'An error occurred while processing your request.');
+    }
+    userInput.value = ""
+  }
+
+  // Function to handle recommended message click
+  function handleRecommendationClick(event) {
+    const recommendedMessage = event.target.textContent;
+    userInput.value = recommendedMessage;
+    userInput.focus();
+  }
+
+  // // Example function to generate a response
+  // function getResponse(message) {
+  //   // Here, you can implement the logic to generate a response from the bot
+  //   // based on the user's input
+  //   return "This is a response from DoctorChatBot.";
+  // }
+
+  // Event listener for login form submission
+  loginForm.addEventListener("submit", handleLogin);
+
+  // Event listener for user input
+  userInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevent form submission
+      handleUserInput();
+    }
+  });
+
+  // Event listener for recommended message click
+  recommendationList.addEventListener("click", handleRecommendationClick);
+  // Event listener for Enter button click
+  enterButton.addEventListener("click", handleUserInput);  
+
+});
